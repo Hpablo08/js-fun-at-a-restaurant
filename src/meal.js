dd89c71c77d46a1590eae850bf4b18d1a2dd8d0c
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-07-06 18:00:41 -0600

    initial meal answers

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,32 @@
-function nameMenuItem() {
+function nameMenuItem(item) {
+return `Delicious ${item}`
+}
+
+function createMenuItem(name, price, type){
+  return{
+  name: name,
+  price: price,
+  type: type,
+}
+}
+
+function addIngredients() {
+
+ingredients.unshift('cheese')
 
 }
+
+
+
+
+
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+   createMenuItem,
+   addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
 }
-
-
