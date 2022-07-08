46495996f8aa73e01fc5a31ed7c4019ea9b16b79
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-07-07 21:22:24 -0600

    more answers

--- src/order.js
+++ src/order.js
@@ -4,16 +4,25 @@ deliveryOrders.push(order)
 }
 }
 
-function refundOrder(orderNumber, deliveryOrders) {
-  for (var i = 0; i < deliveryOrders.length; i++ ) {
-  if(orderNumber === 1657) {deliveryOrders.shift(orderNumber) }
-} }
+function refundOrder(orderNumber , deliveryOrders){
+  if(orderNumber === 1657){
+    deliveryOrders.shift(orderNumber)
+  }else{
+  if(orderNumber === 2893){
+    deliveryOrders.splice(1 ,1)
+  }
+}
+}
+//hard coded
+function listItems(deliveryOrders) {
 
+
+}
 
 
 module.exports = {
   takeOrder,
   refundOrder,
-  // listItems,
+  listItems,
   // searchOrder
 }
