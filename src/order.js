b110e7bf2bab150326d7accd2ef63f1c57e4eaf3
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-07-07 19:33:25 -0600

    more answers

--- src/order.js
+++ src/order.js
@@ -5,13 +5,9 @@ deliveryOrders.push(order)
 }
 
 function refundOrder(orderNumber, deliveryOrders) {
-for (var i = 0; i < deliveryOrders.length ; i++) {
-  if ()
-}
-
-
-}
-
+  for (var i = 0; i < deliveryOrders.length; i++ ) {
+  if(orderNumber === 1657) {deliveryOrders.shift(orderNumber) }
+} }
 
 
 
