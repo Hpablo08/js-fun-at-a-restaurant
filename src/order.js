1963447a4c5f5fd22240bcfd2938db41b5356c09
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-07-07 19:14:01 -0600

    initial answers

--- src/order.js
+++ src/order.js
@@ -1,7 +1,23 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3 ) {
+deliveryOrders.push(order)
+}
+}
+
+function refundOrder(orderNumber, deliveryOrders) {
+for (var i = 0; i < deliveryOrders.length ; i++) {
+  if ()
+}
+
+
+}
+
+
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
+  takeOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 }
