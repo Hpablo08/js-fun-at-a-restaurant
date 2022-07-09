5f04ad5040b57b4b33c5c74b298af62c9d0df48f
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-07-08 21:23:14 -0600

    more answers

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,15 +1,35 @@
 function createRestaurant(name) {
-return{
-  name: name,
-  menus: {breakfast:[], lunch:[], dinner:[], },
-}
+  return {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    },
+  }
 }
 
-function addMenuItem (restaurant, food){
-
-restaurant.menus.lunch.push(food)
-
+function addMenuItem(restaurant, food) {
+if(food.type === 'lunch') {
+  restaurant.menus.lunch.push(food)
+} else if (food.type === 'breakfast') {
+  restaurant.menus.breakfast.push(food)
+} else if(food.type === 'dinner') {
+  restaurant.menus.dinner.push(food)
 }
+}
+//for (var i = 0; i < restaurant.length; i++) {
+// if (restaurant[i].type.includes(lunch)) {
+// console.log(restaurant.menus.lunch.push(food))
+// }
+//console.log(food.type) this will display lunch
+//restaurant.menus.lunch.push(food)
+
+function removeMenuItem (restaurant, food){
+restaurant.menus.breakfast.pop(food)
+return 'No one is eating our Bacon and Eggs Pizza - it has been removed from the breakfast menu!'
+}
+
 
 
 
@@ -36,5 +56,5 @@ restaurant.menus.lunch.push(food)
 module.exports = {
   createRestaurant,
   addMenuItem,
-  // removeMenuItem
+  removeMenuItem
 }
