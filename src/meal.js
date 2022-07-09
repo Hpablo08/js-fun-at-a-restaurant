a0f77f37afa7f367a18d2a4c2cca9a75fc71cd42
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-07-08 20:03:12 -0600

    cleanup

--- src/meal.js
+++ src/meal.js
@@ -1,50 +1,50 @@
 function nameMenuItem(item) {
-return `Delicious ${item}`
+  return `Delicious ${item}`
 
 }
 
-function createMenuItem(name, price, type){
-  return{
-  name: name,
-  price: price,
-  type: type,
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type,
+  }
 }
+
+function addIngredients(toppings, food) {
+  if (!food.includes(toppings)) {
+    return food.push(toppings)
+
+  }
 }
-function addIngredients(toppings, array) {
-    if(!array.includes(toppings)) {
-    return array.push(toppings)
-
-} }
-
-
 
 //method or for loop
 
 
 function formatPrice(price) {
-return `$${price}`
+  return `$${price}`
 }
 
-function decreasePrice(price){
+function decreasePrice(price) {
   var sale = price * .10
   var total = price - sale
   return total
 }
 
-function createRecipe(title, ingredients,type) {
-return {
-  title: title,
-  ingredients: ingredients,
-  type: type,
-}
+function createRecipe(title, ingredients, type) {
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: type,
+  }
 }
 
 
 module.exports = {
   nameMenuItem,
-   createMenuItem,
-   addIngredients,
-   formatPrice,
+  createMenuItem,
+  addIngredients,
+  formatPrice,
   decreasePrice,
   createRecipe
 }
