e44369742f56548883cf92d3172d9cb0a41960a2
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-07-08 19:58:07 -0600

    first answers

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,40 @@
+function createRestaurant(name) {
+return{
+  name: name,
+  menus: {breakfast:[], lunch:[], dinner:[], },
+}
+}
+
+function addMenuItem (restaurant, food){
+
+restaurant.menus.lunch.push(food)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+  createRestaurant,
+  addMenuItem,
   // removeMenuItem
 }
